389a5999d71937742a750d3c7a10dfc7fb20753e
Vintohlet <ruslantuleuoff@gmail.com>
2025-04-17 21:39:35 +0500

    fix: changed model and prompt

--- index.js
+++ index.js
@@ -8,21 +8,21 @@ import messageRoutes from "./routes/message.routes.js"
 
 const app = express();
 const corsOptions = {
-    origin: 'https://game-gpt-frontend.vercel.app', 
+    origin: ['https://game-gpt-frontend.vercel.app', 'http://localhost:5173'], 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true, 
+    credentials: true,
     optionsSuccessStatus: 204,
 };
-app.use(cors(corsOptions))
-app.use(express.json()) 
+app.use(cors(corsOptions));
+app.use(express.json());
 const DB_PASSWORD = process.env.DB_PASSWORD;
 
 const connectionString = `mongodb+srv://user:${DB_PASSWORD}@cluster0.tq3hnbx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 
 async function connectDB() {
     try {
-         await mongoose.connect(connectionString)
-         console.log("Connected to DB!")
+        await mongoose.connect(connectionString)
+        console.log("Connected to DB!")
     } catch (error) {
         console.log("Error to connect DB")
         console.log(error)
@@ -35,5 +35,5 @@ app.use("/messages", messageRoutes)
 app.listen(3333, async() =>{
     await connectDB();
     console.log(`Server on http://localhost:3333`)
-    
+
 })
